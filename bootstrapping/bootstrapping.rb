50fbb61abaef613ac21038c120536d278ac4da34
Thomas Markus <github@thomasmarkus.nl>
2013-07-07 09:38:06 +0200

    rewrite stats to make use of non-parametric tests

--- bootstrapping/bootstrapping.rb
+++ bootstrapping/bootstrapping.rb
@@ -5,7 +5,7 @@ require 'lzma'
 #process files in some datarange
 
 DAY = 24*60*60
-BASE = "http://amphibian.dyndns.org/freenet/tests"
+BASE = ["http://amphibian.dyndns.org/freenet/tests", "http://home.thomasmarkus.nl/freenet/tests"]
 types = ['boot-pull', 'bootstrap', 'long-term-manysingleblocks', 'long-term-mhk', 'long-term-push-pull', 'push-pull', 'seednodes']
 
 now = Time.now - (24*60*60) # go back a day, because we want to be sure that all tests have been run for that day
@@ -28,41 +28,43 @@ while current < now
 	count = 0
 
 	types.each do |type|
-		uri = "#{BASE}/#{type}/" + current.strftime("%Y/%m/%d")
+		BASE.each do |base|
+			uri = "#{base}/#{type}/" + current.strftime("%Y/%m/%d")
 
-		begin
-			open(uri).each_line do |line|
-				result = line.scan(/log(\.failed)?\.lzma">(.*\.log(\.failed)?\.lzma)<\/a>/)
-				if (result.first && result.first.size == 3)
-					filename = uri + "/" + result.first[1]
-			
-					puts filename
+			begin
+				open(uri).each_line do |line|
+					result = line.scan(/log(\.failed)?\.lzma">(.*\.log(\.failed)?\.lzma)<\/a>/)
+					if (result.first && result.first.size == 3)
+						filename = uri + "/" + result.first[1]
+		
+						puts filename
 
-					#open file
-					time = nil 
-					LZMA.decompress(open(filename, "rb").read).split("\n").each do |line|
-						reg_exp = /(\d+) : seeds: (\d+), connected: (\d+) opennet: peers: (\d+), connected: (\d+)/
-						if (line =~  reg_exp)
-							time, seeds, seeds_connected, opennet_peers, opennet_peers_connected = line.scan(reg_exp)[0]
+						#open file
+						time = nil 
+						LZMA.decompress(open(filename, "rb").read).split("\n").each do |line|
+							reg_exp = /(\d+) : seeds: (\d+), connected: (\d+) opennet: peers: (\d+), connected: (\d+)/
+							if (line =~  reg_exp)
+								time, seeds, seeds_connected, opennet_peers, opennet_peers_connected = line.scan(reg_exp)[0]
+							end
 						end
+
+						total += time.to_i
+						count += 1		
 					end
-
-					total += time.to_i
-					count += 1		
 				end
-			end
 
-		rescue => e # usually a 404 is triggered here if the test wasn't run on that day
-		 puts e.message
-		 #do nothing
+			rescue => e # usually a 404 is triggered here if the test wasn't run on that day
+			 puts e.message
+			 #do nothing
+			end
+		end
+	
+		if (total > 1)
+			output_line = current.strftime("%Y-%m-%d") + "\t" + (total.to_f / count).to_s + "\n"
+			data.write(output_line)
 		end
 	end
-	
-	if (total > 1)
-		output_line = current.strftime("%Y-%m-%d") + "\t" + (total.to_f / count).to_s + "\n"
-		data.write(output_line)
-	end
-	
+
 	current += DAY
 end
 
